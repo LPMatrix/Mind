6fa48819236c79fba072b73f4aff2d9f83e57c1e
LPMatrix <mubaraqsanusi908@gmail.com>
2018-12-23 22:42:55 +0100

    added clock

--- rules/rules.js
+++ rules/rules.js
@@ -5,12 +5,36 @@ document.getElementById('title').innerHTML = data[random].title;
 document.getElementById('content').innerHTML = data[random].content;
 document.getElementById('container').style.backgroundColor = color
 
-var d = new Date()
-var hour = d.getHours()
-var mins = d.getMinutes()
-var sec = d.getSeconds()
-var time = hour+':'+mins+':'+sec
-document.getElementById('time').innerHTML = time
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("time").innerText = time;
+    document.getElementById("time").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
+
 document.getElementById('button').onclick = function () {
 	location.reload();
 }
