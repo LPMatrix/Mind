0b9cd586b2e1ecbad9f4f12189daed7e4e50db7f
LPMatrix <mubaraqsanusi908@gmail.com>
2018-12-23 22:09:57 +0100

    updated UI

---
+++ quotes/quotes.js
@@ -0,0 +1,10 @@
+var random = Math.floor(Math.random() * 12);
+var colors =["#AA00FF","#C51162","#D50000","#2962FF","#304FFE","#6200EA","#00BFA5","#00B8D4","#0091EA","#AEEA00","#64DD17","#00C853","#FF6D00","#FFAB00","#FFD600","#212121","#3E2723","#DD2C00","#263238"]
+color = colors[Math.floor(Math.random()*colors.length)]
+document.getElementById('title').innerHTML = data[random].quote;
+document.getElementById('content').innerHTML = data[random].author;
+document.getElementById('container').style.backgroundColor = color
+
+document.getElementById('button').onclick = function () {
+	location.reload();
+}
