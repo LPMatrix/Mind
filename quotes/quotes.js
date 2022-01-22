733d96da84a6c6550e571e198a3e9150bf68487c
Sanusi Mubaraq <mubaraq@Sanusis-MacBook-Pro.local>
2022-01-22 06:22:30 +0100

    programming quotes removed

--- quotes/quotes.js
+++ quotes/quotes.js
@@ -2,7 +2,7 @@ var random = Math.floor(Math.random() * data.length);
 var colors =["#AA00FF","#C51162","#D50000","#2962FF","#304FFE","#6200EA","#00BFA5","#00B8D4","#0091EA","#AEEA00","#64DD17","#00C853","#FF6D00","#FFAB00","#FFD600","#212121","#3E2723","#DD2C00","#263238"]
 color = colors[Math.floor(Math.random()*colors.length)]
 document.getElementById('title').innerHTML = data[random].quote;
-document.getElementById('content').innerHTML = data[random].author;
+document.getElementById('content').innerHTML = String(data[random].author);
 document.getElementById('container').style.backgroundColor = color
 
 
